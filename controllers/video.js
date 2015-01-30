e0f7a1be754e34f3087673af9f16809d24ad76c4
Paterna <roberto.paterna@gmail.com>
2015-01-30 20:13:48 +0100

    veeeenga

--- controllers/video.js
+++ controllers/video.js
@@ -10,13 +10,12 @@ exports.view = function(req, res) {
 exports.upload = function(req, res, next) {
 	
     var video_name = req.body.nombre;
-    console.log('Nombre del vídeo: ' + video_name);
+    // console.log('Nombre del vídeo: ' + video_name);
 
 	var fstream;
     req.pipe(req.busboy);
 
     req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("file: " + JSON.stringify(file));
         console.log("Subiendo vídeo: " + filename);
 
         var video = new models.Video({
@@ -30,11 +29,15 @@ exports.upload = function(req, res, next) {
             }
             console.log('Vídeo subido: ' + JSON.stringify(video));
             res.locals.video = {
+                _id: video._id,
                 name: video.name
             };
             extension_check = /(.*)\.(.*)/;
             var video_extension = extension_check.exec(filename);
-            console.log('Extensión: ' + video_extension[2]);
+            res.locals.extension = video_extension[2];
+
+            console.log('Vídeo: %s', JSON.stringify(res.locals.video));
+            console.log('Extensión: %s', res.locals.extension);
 
             video_name = video_file._id + filename
             fstream = fs.createWriteStream('/mnt/nas/' + video_file._id + "." + video_extension[2]);
